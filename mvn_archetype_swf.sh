6f54f1b7de0035a9abc61425c2645344ad32dd03
tottokug <magcot.com+git@gmail.com>
2013-06-17 19:27:53 +0900

    firstcommit

---
+++ mvn_archetype_swf.sh
@@ -0,0 +1,22 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: archetype:swf.sh
+# 
+#         USAGE: ./archetype:swf.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 2013/06/17 19時07分12秒 JST
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+
